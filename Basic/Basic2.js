970dd5e7607550df5adaf29ef35419feaf1f36b2
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-08-11 12:54:05 +0530

    var,let,const

---
+++ Basic/Basic2.js
@@ -0,0 +1,46 @@
+// var a = 10;
+// console.log("outer value of a = ",a);
+
+// let b = 23;
+// console.log("outer value of b = ",b);
+
+
+// {
+//     console.log("inner value of a = ",a);
+//     console.log("inner value of b = ",b);
+
+
+//     var c = 22;
+//     console.log("inner value of c = ",c);
+    
+//     let d = 56;
+//     console.log("inner value of d = ",d);
+    
+    
+// }
+
+// console.log("outer value of c = ",c);
+// console.log("outer value of d = ",d);
+
+
+// var a = 10;
+// console.log("value of a = ",a);
+
+// var a = 30;
+// console.log("value of a = ",a);
+
+
+// let a = 10;
+// console.log("value of a = ",a);
+
+// a = 20;
+// console.log("value of a = ",a);
+
+const pi = 3.14;
+console.log("value of pi = ",pi);
+
+pi = 3.15;
+console.log("value of pi = ",pi);
+
+
+
