da0ba228e23e2f8c9d9550aaf440e30554f51ec4
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-08-11 12:34:03 +0530

    basic

---
+++ Basic/Basic1.js
@@ -0,0 +1,40 @@
+// console.log("Hello world!");
+// console.info("Information function called")
+// console.warn("warning function called");
+// console.error("error function called");
+
+// Datatype : number , string , boolean , undefined , null
+
+
+a = "10";
+console.log("value of a = ",a);
+console.log("type of a = ",typeof(a));
+
+b = 14.5;
+console.log("value of b = ",b);
+console.log("type of b = ",typeof(b));
+
+c = "Ramya";
+console.log("value of c = ",c);
+console.log("type of c = ",typeof(c));
+
+isAlive = true;
+console.log("value of isAlive = ",isAlive);
+console.log("type of isAlive = ",typeof isAlive);
+
+var x;
+console.log("value of x = ",x);
+console.log("type of x = ",typeof x);
+
+var y = null;
+console.log("value of y = ",y);
+console.log("type of y = ",typeof y);
+
+
+
+
+
+
+
+
+
