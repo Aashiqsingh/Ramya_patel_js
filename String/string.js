892e6944984dda285025bc3486293f2238124fd8
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-09-01 14:42:41 +0530

    string

---
+++ String/string.js
@@ -0,0 +1,120 @@
+
+// console.log(str);
+// console.log(typeof str);
+
+// console.log(str.length);
+
+// var x = str.charAt(1)
+// console.log(x);
+
+// var x = str.charCodeAt(26)
+// console.log(x);
+
+// var x = str.codePointAt(26)
+// console.log(x);
+
+// str2 = "dhiraj poojara"
+
+// var x = str.concat(str2)
+// var x = str + str2
+// console.log(x);
+
+
+
+
+// // var x = str.replace("o","s")
+// var x = str.replace("o","seen")
+
+// // var x = str.replaceAll("c","s")
+// console.log(str);
+
+// console.log(x);
+
+
+// var x = str.repeat(12)
+// console.log(x);
+
+
+// var x = str.indexOf("o")
+// console.log(x);
+
+
+// var x = str.includes("soft")
+// console.log(x);
+
+
+// var x = str.startsWith("Roy")
+// console.log(x);
+
+
+// var x = str.endsWith("td.")
+// console.log(x);
+
+var str = "        Royal technosoft pvt ltd.        ";
+console.log(str);
+console.log(str.length);
+
+var x = str.trimStart()
+console.log(x);
+
+
+// console.log(str.trim().length);
+
+
+
+
+
+
+
+
+
+
+// var x = str.split("")
+// console.log(x);
+// var ans = x.toString().replaceAll(",","")
+// console.log(ans);
+
+
+// var x = str.toString()
+// console.log(x);
+
+
+// x = str.substring(1,4)
+// console.log(x);
+
+
+
+// var x = str.split("t")
+// x = str.split("");
+// console.log(x);
+// console.log(x[0]);
+
+
+
+
+
+
+// var x = str.slice(1)
+// var x = str.slice(1,4)
+
+// var x = str.slice(-6)
+// var x = str.slice(-6 , -2)
+
+
+// console.log(x);
+
+
+// var x = str.toLocaleLowerCase()
+
+// var x = str.toUpperCase()
+// console.log(x);
+
+
+
+// user = username without spacing 
+// ex : aashiqsingh
+
+// @aashiqsingh11 
+
+
+
