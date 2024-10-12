52e662d7a1754fdfd2d7967f6b7ced5d046dfa6d
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-12 15:13:31 +0530

    arrow function

---
+++ ArrowFunction/ArrowFun1.js
@@ -0,0 +1,32 @@
+const demo = ()=>{
+    console.log("Function is called...");
+    
+}
+
+
+// demo()
+
+const add = (a,b)=>{
+    console.log("Addition = ",a+b);
+    
+}
+
+add(5,6)
+
+
+const test = ()=>{
+
+    return "test function is called..."
+}
+
+var x = test()
+console.log(x);
+
+
+const mul = (x,y)=>{
+
+    return x*y;
+}
+
+var x = mul(4,5)
+console.log("multiply = ",x);
