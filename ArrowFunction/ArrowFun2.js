787e4621176056d57a0241010e37a43e95d3d2ef
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-10-19 15:27:08 +0530

    arrow function and ternary operator

---
+++ ArrowFunction/ArrowFun2.js
@@ -0,0 +1,58 @@
+// const demo = ()=>{
+
+//     console.log("hello world");
+    
+// }
+
+
+// demo()
+
+// const isEven = (x)=>{
+
+//     // return x % 2 == 0 ? "Even" : "Odd";
+//     if(x % 2 == 0){
+//         return "Even";
+//     }
+//     else{
+//         return "Odd";
+//     }
+// }
+
+
+// var x = isEven(12)
+// console.log(x);
+
+
+// const isEven = (x)=> x % 2 == 0 ? "Even" : "Odd";
+
+// var x = isEven(22)
+// console.log(x);
+
+
+var a = 70;
+var b = 60;
+var c = 50;
+
+var x = a > b ? (a > c ? a : c) : (b > c ? b : c);
+console.log(x + " is bigger");
+
+
+
+// a > b ? console.log("A is bigger",a) : console.log("B is bigger",b);
+// var x = a > b ? "A is bigger" : "B is bigger";
+// console.log(x);
+
+
+// var x = a > b ? a : b;
+// console.log(x , "is bigger");
+
+
+
+
+// if(a > b){
+//     console.log(a);
+    
+// }
+// else{
+//     console.log(b);
+// }
