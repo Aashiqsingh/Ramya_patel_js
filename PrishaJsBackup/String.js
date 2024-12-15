4021f9041b81e73b98d7150d73dfa06090f29568
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-15 10:59:31 +0530

    prishabhatt

---
+++ PrishaJsBackup/String.js
@@ -0,0 +1,90 @@
+
+var str2 = " PRisha bhatt"
+
+// console.log(str);
+// console.log(str.length);
+
+
+// var x = str.charAt(6)
+// console.log(x);
+
+// var x = str.charCodeAt(0)
+// console.log(x);
+
+
+// str.codePointAt()
+
+// var x = str.concat(str2)
+// var x = str + str2;
+// console.log(x);
+
+// var x = str.endsWith("l.")
+// console.log(x);
+
+// var x = str.startsWith("a")
+// console.log(x);
+
+
+// var x = str.includes("techsoft")
+// console.log(x);
+
+// var x = str.indexOf("t")
+// console.log(x);
+
+// var x = str.repeat(4)
+// console.log(x);
+
+// var x = str.replace("o","z")
+// var x = str.replaceAll("o","z")
+// var x = str.replace("soft","h")
+// console.log(x);
+
+// var x = str.slice(1)
+// var x = str.slice(1,4)
+// var x = str.slice(-4)
+// var x = str.slice(-4 , -1)
+
+
+// console.log(x);
+
+var str = "      Royal technosoft               pvt ltd.        ";
+console.log(str.length);
+
+
+// var x = str.split(" ")
+// var x = str.split("o")
+// var x = str.split("")
+
+
+// // console.log(x);
+// for(let i=0;i<x.length;i++){
+//     console.log(x[i]);
+    
+// }
+
+// str = str.toUpperCase()
+// str = str.toLocaleLowerCase()
+
+// console.log(str);
+
+// var x = str.trim()
+// // var x = str.trimStart()
+// // var x = str.trimEnd()
+
+// console.log(x.length);
+
+
+// user -> userName without spacing  prishabhatt 
+
+// @prishabhatt11
+
+// var str = prompt("Enter your username without spacing")
+
+// var anchor = "@";
+
+// var x = anchor.concat(str)
+// // var y = x.concat(str.length)
+// console.log(x + str.length);
+
+// var ans = "@" + str + str.length
+// console.log(ans);
