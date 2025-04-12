314b6e00188300d39cdc3967430d4a03f8a178c7
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-04-12 12:41:39 +0530

    slieder

---
+++ slider/slider.js
@@ -0,0 +1,24 @@
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+var images = ["img1.png","img2.webp","img3.jpg","img4.jpg","img5.jpg"]
+var img = document.getElementById("img")
+
+var currentIndex = 0;
+prev.addEventListener("click",()=>{
+
+                    //    ( 3 - 1 + 5)% 5
+    currentIndex = (currentIndex - 1 + images.length) % images.length
+    console.log(currentIndex);
+
+    img.src = "../Image/" + images[currentIndex]
+    
+})
+
+next.addEventListener("click",()=>{
+                        // 2 + 1 % 5
+    currentIndex = (currentIndex + 1) % images.length
+    console.log(currentIndex);
+    img.src = "../Image/" + images[currentIndex]
+    
+})
