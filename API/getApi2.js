195bccbf4a6f464c375a6f28fc310e63f82a56ee
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-05-04 14:24:44 +0530

    get api

---
+++ API/getApi2.js
@@ -0,0 +1,41 @@
+let thead = document.getElementById("thead")
+thead.style.display = "none"
+
+const getData = async () => {
+    const response = await fetch("https://node5.onrender.com/user/user", {
+        method: "GET"
+    })
+
+    // console.log(response);
+    const res = await response.json()
+    console.log(res.data);
+
+
+    thead.style.display = ""
+    let tbody = document.getElementById("tbody")
+
+
+    for (let i = 0; i < res.data.length; i++) {
+        let tr = document.createElement("tr")
+        let idTd = document.createElement("td")
+        let nameTd = document.createElement("td")
+        let emailTd = document.createElement("td")
+        let passwordTd = document.createElement("td")
+        let isActiveTd = document.createElement("td")
+
+        idTd.innerHTML = res.data[i]._id
+        nameTd.innerHTML = res.data[i].name
+        emailTd.innerHTML = res.data[i].email
+        passwordTd.innerHTML = res.data[i].password
+        isActiveTd.innerHTML = res.data[i].isActive
+
+
+
+        tbody.appendChild(tr)
+        tr.appendChild(idTd)
+        tr.appendChild(nameTd)
+        tr.appendChild(emailTd)
+        tr.appendChild(passwordTd)
+        tr.appendChild(isActiveTd)
+    }
+}
